91f6b3155cfd3399b46a395c4faecc2c1bedd934
Gaborpg <gaborpg@gmail.com>
2020-09-26 14:44:18 +0200

    angularjson update, panel component add

---
+++ angularall/src/app/shared/components/panel/panel.component.ts
@@ -0,0 +1,35 @@
+import {
+  Component,
+  ElementRef,
+  Input,
+  OnInit,
+  Renderer2,
+  TemplateRef,
+  ViewChild,
+} from '@angular/core';
+
+@Component({
+  selector: 'myapp-panel',
+  templateUrl: './panel.component.html',
+  styleUrls: ['./panel.component.scss'],
+})
+export class PanelComponent implements OnInit {
+  @ViewChild('panel', { static: true }) panel: ElementRef;
+  @Input() color: string;
+  @Input() height?: string;
+  @Input() header: TemplateRef<any>;
+  @Input() footer: TemplateRef<any>;
+
+  constructor(private renderer: Renderer2) {}
+
+  ngOnInit(): void {
+    this.renderer.setStyle(
+      this.panel.nativeElement,
+      'background-color',
+      this.color ? this.color : '#ffffff'
+    );
+    if (this.height) {
+      this.renderer.setStyle(this.panel.nativeElement, 'height', this.height);
+    }
+  }
+}
--- angularall/src/app/shared/shared.module.ts
+++ angularall/src/app/shared/shared.module.ts
@@ -6,9 +6,18 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { InputComponent } from './components/input/input.component';
 import { CheckboxComponent } from './components/checkbox/checkbox.component';
 import { EnumkeyvaluePipe } from './pipes/enumkeyvalue.pipe';
+import { ButtonComponent } from './components/button/button.component';
+import { PanelComponent } from './components/panel/panel.component';
 
 @NgModule({
-  declarations: [FormFieldComponent, InputComponent, CheckboxComponent, EnumkeyvaluePipe],
+  declarations: [
+    FormFieldComponent,
+    InputComponent,
+    CheckboxComponent,
+    EnumkeyvaluePipe,
+    ButtonComponent,
+    PanelComponent,
+  ],
   imports: [CommonModule, BrowserModule, FormsModule, ReactiveFormsModule],
   exports: [
     CommonModule,
@@ -19,6 +28,8 @@ import { EnumkeyvaluePipe } from './pipes/enumkeyvalue.pipe';
     FormFieldComponent,
     InputComponent,
     CheckboxComponent,
+    ButtonComponent,
+    PanelComponent,
   ],
 })
 export class SharedModule {}
